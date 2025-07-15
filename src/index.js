f6ad0d592902d4724d06046a132fe49f8fcc8fb8
Daya Juang Mahaputra <dayajuangmahaputra@gmail.com>
2025-07-15 11:00:16 +0900

    [learn] adding react fragment

--- src/index.js
+++ src/index.js
@@ -18,12 +18,19 @@ export function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {pizzas.length > 0 && (
-        <ul className="pizzas">
-          {pizzas.map((pizza, i) => (
-            <Pizza key={i} pizzaObj={pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone stove, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza, i) => (
+              <Pizza key={i} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
       )}
     </main>
   );
